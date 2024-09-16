daa2e62b0f3b4cc1bffdd111603ad3f6b811a798
monisharmaofficial <monika2022.sharma@gmail.com>
2024-09-16 17:18:10 +0530

    change

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Todo from "./Components/Todo"
+
+ 
+ const App = () => {
+   return (
+     <div className="bg-stone-900 grid min-h-screen">
+     <Todo/>
+     </div>
+   )
+ }
+ 
+ export default App
+ 
